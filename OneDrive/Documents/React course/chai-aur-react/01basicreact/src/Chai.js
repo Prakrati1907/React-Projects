e8e4b703bddddca678caa3309fe586a7fc1cf2db
Prakrati Gupta <120261907+Prakrati1907@users.noreply.github.com>
2025-01-24 01:24:24 +0530

    Delete OneDrive/Documents/React course/chai-aur-react directory

--- OneDrive/Documents/React course/chai-aur-react/01basicreact/src/Chai.js
+++
@@ -1,6 +0,0 @@
-function Chai(){
-    return(
-        <h3>chai is ready brother</h3>
-    )
-}
-export default Chai;
--- OneDrive/Documents/React course/chai-aur-react/01vitereact/src/Chai.jsx
+++
@@ -1,6 +0,0 @@
-function Chai(){
-    return(
-        <h1>Here the Chai is ready</h1>
-    )
-}
-export default Chai;
